b248144a0f825be18bc52424fe8340900af2e4e4
Dalton Walker <dev@daltonwalker.com>
2024-04-16 22:08:27 -0400

    init

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
+import { defineConfig } from 'vitest/config';
+import Icons from 'unplugin-icons/vite';
+
+export default defineConfig({
+	plugins: [
+		enhancedImages(),
+		sveltekit(),
+		Icons({
+			compiler: 'svelte'
+		})
+	],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
