ffb27da481d44c774b03871fc370468a48bb1cad
Dalton Walker <dev@daltonwalker.com>
2024-04-20 17:25:15 -0400

    finish for dinner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../app.pcss';
 	import { ModeWatcher } from 'mode-watcher';
 	import { AspectRatio } from '$lib/components/ui/aspect-ratio';
-	// import logo from '$lib/assets/logo.jpg?enhanced';
+	import logo from '$lib/images/logo.jpg?enhanced';
 	import * as Menubar from '$lib/components/ui/menubar/index.js';
 	import DarkmodeSwitch from '$lib/components/ui/darkmode-switch/DarkmodeSwitch.svelte';
 	import ProfileDropdown from '$lib/components/ui/profile-dropdown/ProfileDropdown.svelte';
@@ -12,59 +12,24 @@
 </script>
 
 <ModeWatcher defaultMode={'dark'} />
-<div class="flex h-fit max-h-fit flex-col items-center justify-between">
+<div class=" top-bar bg-amber-50 flex flex-col items-center justify-between">
 	<div id="nav-bar" class="sticky flex w-full items-center justify-between p-2">
 		<div class="flex items-center">
 			<Button href="/" class=" m-0 h-[60px] w-[60px] bg-transparent p-0">
-				<!-- <enhanced:img
+				<enhanced:img
 					src={logo}
 					alt="Mithral Labs logo"
 					class="h-full w-full rounded-md object-cover"
-				/> -->
+				/>
 			</Button>
 			<div class=" flex items-end px-4">
-				<h1 class="scroll-m-20 align-baseline text-4xl font-normal tracking-tight lg:text-5xl">
-					Git Chef
-				</h1>
+				<h1 class="title-font scroll-m-20 align-baseline text-4xl lg:text-5xl">Git Chef</h1>
 			</div>
 		</div>
 		<div class="mx-2 flex gap-2">
 			{#if isLoggedIn}
 				<Button href="/profile/dalton">My Recipes</Button>
 			{/if}
-			<Menubar.Root>
-				<Menubar.Menu>
-					<Menubar.Trigger>Solutions</Menubar.Trigger>
-					<Menubar.Content>
-						<Menubar.Item>Testing/Evaluation</Menubar.Item>
-					</Menubar.Content>
-				</Menubar.Menu>
-				<Menubar.Menu>
-					<Menubar.Trigger>Documentation</Menubar.Trigger>
-					<Menubar.Content>
-						<Menubar.Item>Computer Vision</Menubar.Item>
-						<Menubar.Item>Natural Language Processing</Menubar.Item>
-						<Menubar.Item>Reinforcement Learning</Menubar.Item>
-						<Menubar.Item>Classical Machine Learning</Menubar.Item>
-						<Menubar.Separator />
-						<Menubar.Item>FAQ</Menubar.Item>
-					</Menubar.Content>
-				</Menubar.Menu>
-				<Menubar.Menu>
-					<Menubar.Trigger>Contact</Menubar.Trigger>
-					<Menubar.Content>
-						<Menubar.Separator />
-						<Menubar.Item>Email</Menubar.Item>
-					</Menubar.Content>
-				</Menubar.Menu>
-				<Menubar.Menu>
-					<Menubar.Trigger>Pricing</Menubar.Trigger>
-					<Menubar.Content>
-						<Menubar.Item>Free</Menubar.Item>
-						<Menubar.Item>Enterprise</Menubar.Item>
-					</Menubar.Content>
-				</Menubar.Menu>
-			</Menubar.Root>
 			{#if !isLoggedIn}
 				<Button href="/sign-in">Sign In</Button>
 				<Button variant="secondary" href="/sign-up">Sign Up</Button>
@@ -77,3 +42,15 @@
 	<Separator />
 </div>
 <slot />
+
+<style>
+	.title-font {
+		font-family: 'Playfair Display', serif;
+		font-optical-sizing: auto;
+		font-style: normal;
+	}
+
+	.top-bar {
+		height: 80px;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,28 +9,32 @@
 	export let data: PageData;
 </script>
 
-<Tabs.Root>
-	<Tabs.List class="grid w-full grid-cols-2">
-		<Tabs.Trigger value="all">All</Tabs.Trigger>
-		<Tabs.Trigger value="favorites">Favorites</Tabs.Trigger>
-	</Tabs.List>
-	<Tabs.Content value="all">
-		<div class="recipe-grid">
-			{#each data.recipes as recipe}
-				<a href="/recipes/{recipe.name}">
+<div class="tab-container bg-amber-50">
+	<Tabs.Root class=" ">
+		<Tabs.List class="grid w-full grid-cols-2  bg-amber-100">
+			<Tabs.Trigger class="data-[state=active]:bg-amber-200" value="all">All</Tabs.Trigger>
+			<Tabs.Trigger class="data-[state=active]:bg-amber-200" value="favorites"
+				>Favorites</Tabs.Trigger
+			>
+		</Tabs.List>
+		<Tabs.Content value="all">
+			<div class=" bg-amber-50 recipe-grid">
+				{#each data.recipes as recipe}
+					<a href="/recipes/{recipe.name}">
+						<RecipeSmallCard {recipe} />
+					</a>
+				{/each}
+			</div>
+		</Tabs.Content>
+		<Tabs.Content value="favorites">
+			<div class="recipe-grid">
+				{#each data.recipes as recipe}
 					<RecipeSmallCard {recipe} />
-				</a>
-			{/each}
-		</div>
-	</Tabs.Content>
-	<Tabs.Content value="favorites">
-		<div class="recipe-grid">
-			{#each data.recipes as recipe}
-				<RecipeSmallCard {recipe} />
-			{/each}
-		</div>
-	</Tabs.Content>
-</Tabs.Root>
+				{/each}
+			</div>
+		</Tabs.Content>
+	</Tabs.Root>
+</div>
 
 <style>
 	.recipe-grid {
@@ -40,5 +44,10 @@
 		padding: 10px;
 		width: 100%;
 		max-width: 100vw;
+	}
+
+	.tab-container {
+		width: 100%;
+		height: calc(100vh - 80px);
 	}
 </style>
